db3626873e578ebabe6e824b7289a74632c7a697
gean123902 <105463245+gean123902@users.noreply.github.com>
2023-02-04 01:47:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,2 @@
+window.alert('Essa página foi criada para um projeto escolar do Instituto de Ensino Sagrada Família, por parte do itinerário Ciências da Natureza @2022 - autores- gean12390 e o Professor Thiago SantAnna')
+
